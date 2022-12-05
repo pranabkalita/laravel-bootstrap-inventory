91fafd6d6acad1cbe4fe1cadc57ab307db8215ca
Pranab Kalita <pavankalita@gmail.com>
2022-12-05 12:30:10 +0530

    Implemented Reset Password with Bootstrap

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,47 +1,63 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+    <div class="card-body login-card-body">
+        <p class="login-box-msg">You are only one step a way from your new password, recover your password now.</p>
 
-        <form method="POST" action="{{ route('password.store') }}">
+        <form action="{{ route('password.store') }}" method="post">
             @csrf
 
             <!-- Password Reset Token -->
             <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <!-- Email Address -->
-            <div>
-                <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <div class="input-group mb-3">
+                <input type="text" name="email" class="form-control @error('email') is-invalid @enderror"
+                    placeholder="Email" value="{{ old('email', $request->email) }}">
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                    </div>
+                </div>
+
+                @error('email')
+                    <span class="error invalid-feedback">{{ $message }}</span>
+                @enderror
             </div>
+            <div class="input-group mb-3">
+                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
+                    placeholder="Password">
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                    </div>
+                </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
-                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required />
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                @error('password')
+                    <span class="error invalid-feedback">{{ $message }}</span>
+                @enderror
             </div>
+            <div class="input-group mb-3">
+                <input type="password" name="password_confirmation"
+                    class="form-control @error('password_confirmation') is-invalid @enderror"
+                    placeholder="Confirm Password">
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                    </div>
+                </div>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
-
-                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                @error('password_confirmation')
+                    <span class="error invalid-feedback">{{ $message }}</span>
+                @enderror
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-primary-button>
-                    {{ __('Reset Password') }}
-                </x-primary-button>
+            <div class="row">
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary btn-block">Change password</button>
+                </div>
+                <!-- /.col -->
             </div>
         </form>
-    </x-auth-card>
+
+        <p class="mt-3 mb-1">
+            <a href="login.html">Login</a>
+        </p>
+    </div>
 </x-guest-layout>
